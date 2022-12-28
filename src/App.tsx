16ef1b97591914effaa0fefe2fc51bbe629f5640
Phongsavanh Mongkhonvilay <azulatech@PhongsavanhsMBP.lan>
2022-12-27 18:42:43 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [topNumber, setTopNumber] = useState(Math.floor(Math.random() * 10))
+const [bottomNumber, setBottomNumber] = useState(Math.floor(Math.random() * 10))
+const [result, setResult] = useState("")
+
+
+
+const handleChange = (e: React.FormEvent<HTMLInputElement>)=>{
+  setResult(e.currentTarget.value)
+
+
+  }
+
+
+useEffect(() => {
+  let resultNumber: number = +result
+
+  if(topNumber*bottomNumber===resultNumber){
+    setTopNumber(Math.floor(Math.random() * 10))
+    setBottomNumber(Math.floor(Math.random() * 10))
+    setResult("")
+
+  }
+}, [result])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+             <div className="math"> 
+             <div className="card">
+                
+              <div className="sign">X</div>
+              <div className="numbers">
+              <div className="top">{topNumber}</div>
+              <div className="bottom">{bottomNumber}</div>
+                </div>
+            
+              </div><hr />
+              <div className="result">
+                <input type="text" 
+                value={result}
+                onChange={handleChange}
+                />
+                </div>
+         
+              </div>
+              
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
